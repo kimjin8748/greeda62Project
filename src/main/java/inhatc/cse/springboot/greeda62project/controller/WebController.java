ce0ce38d282f2f9c8b7d1bb75679894b378cba3a
김용진 <kimjin8748@gmail.com>
2024-04-02 15:11:23 +0900

    index controller 생성

--- src/main/java/inhatc/cse/springboot/greeda62project/controller/WebController.java
+++ src/main/java/inhatc/cse/springboot/greeda62project/controller/WebController.java
@@ -1,6 +1,7 @@
 package inhatc.cse.springboot.greeda62project.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -8,8 +9,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class WebController {
 
-    @RequestMapping(value="/html2", method = RequestMethod.GET)
-    public String api_test(){
+    @GetMapping("/")
+    public String index(){
         return "index";
     }
 }
