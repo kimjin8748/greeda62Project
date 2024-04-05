7242a19374b665e637c281797237d07a7b71d247
김용진 <kimjin8748@gmail.com>
2024-04-05 11:20:45 +0900

    login DB 연동 클래스

---
+++ src/main/java/inhatc/cse/springboot/greeda62project/dto/MemberDTO.java
@@ -0,0 +1,17 @@
+package inhatc.cse.springboot.greeda62project.dto;
+
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Builder
+public class MemberDTO {
+
+    private String id;
+    private String name;
+    private String email;
+    private String address;
+
+}
