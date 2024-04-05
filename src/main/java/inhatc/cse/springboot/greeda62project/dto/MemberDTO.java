652d25d9e9e6f02fbd4f3706ef72bf9e1e85ff9c
김용진 <kimjin8748@gmail.com>
2024-04-05 14:38:07 +0900

    로그인 화면 구성을 위해 임시 주석 처리

--- src/main/java/inhatc/cse/springboot/greeda62project/dto/MemberDTO.java
+++ src/main/java/inhatc/cse/springboot/greeda62project/dto/MemberDTO.java
@@ -1,17 +1,17 @@
-package inhatc.cse.springboot.greeda62project.dto;
-
-import lombok.*;
-
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-@ToString
-@Builder
-public class MemberDTO {
-
-    private String id;
-    private String name;
-    private String email;
-    private String address;
-
-}
+//package inhatc.cse.springboot.greeda62project.dto;
+//
+//import lombok.*;
+//
+//@Data
+//@NoArgsConstructor
+//@AllArgsConstructor
+//@ToString
+//@Builder
+//public class MemberDTO {
+//
+//    private String id;
+//    private String name;
+//    private String email;
+//    private String address;
+//
+//}
