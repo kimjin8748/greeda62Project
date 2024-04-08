da02c0fcd54f1d7f2ec402c5175988a2471341e1
김용진 <kimjin8748@gmail.com>
2024-04-08 10:54:42 +0900

    Optional 추가

--- src/main/java/inhatc/cse/springboot/greeda62project/repository/MemberRepository.java
+++ src/main/java/inhatc/cse/springboot/greeda62project/repository/MemberRepository.java
@@ -3,6 +3,9 @@ package inhatc.cse.springboot.greeda62project.repository;
 import inhatc.cse.springboot.greeda62project.entity.MemberEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<MemberEntity, String> {
 
+    Optional <MemberEntity> findById(String id);
 }
