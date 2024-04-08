8f2c0d4b6e74781c65687c3c5602ae7feb39c141
김용진 <kimjin8748@gmail.com>
2024-04-08 10:57:54 +0900

    login mapping 추가(DB연동 완료)

--- src/main/java/inhatc/cse/springboot/greeda62project/controller/MemberController.java
+++ src/main/java/inhatc/cse/springboot/greeda62project/controller/MemberController.java
@@ -3,11 +3,10 @@ package inhatc.cse.springboot.greeda62project.controller;
 import inhatc.cse.springboot.greeda62project.dao.MemberDAO;
 import inhatc.cse.springboot.greeda62project.dto.MemberDTO;
 import inhatc.cse.springboot.greeda62project.service.MemberService;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class MemberController {
@@ -20,17 +19,33 @@ public class MemberController {
     }
 
     @GetMapping("/member")
-    public String member() {
+    public String memberSignup() {
         return "member";
     }
 
     @PostMapping("/member")
     public MemberDTO createMember(MemberDTO memberDTO){
         String id = memberDTO.getId();
+        String password = memberDTO.getPassword();
         String name = memberDTO.getName();
         String email = memberDTO.getEmail();
         String address = memberDTO.getAddress();
 
-        return memberService.saveMember(id, name, email, address);
+        return memberService.saveMember(id, password, name, email, address);
+    }
+
+    @PostMapping("/login")
+    public String login(@RequestParam("id") String id, @RequestParam("password") String password, HttpSession session){
+        MemberDTO memberDTO = new MemberDTO();
+        memberDTO.setId(id);
+        memberDTO.setPassword(password);
+
+        MemberDTO loginResult = memberService.login(memberDTO);
+        if(loginResult != null) {
+            session.setAttribute("id", loginResult.getId());
+            return "index";
+        } else {
+            return "member";
+        }
     }
 }
