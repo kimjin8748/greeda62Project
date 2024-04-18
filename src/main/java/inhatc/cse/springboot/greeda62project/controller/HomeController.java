6aa1c672608a00df1fae97620f2e1c7b2774bc96
김용진 <kimjin8748@gmail.com>
2024-04-18 16:49:47 +0900

    main으로 이름 변경

--- src/main/java/inhatc/cse/springboot/greeda62project/controller/HomeController.java
+++ src/main/java/inhatc/cse/springboot/greeda62project/controller/HomeController.java
@@ -7,8 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
 
     @GetMapping("/")
-    public String index(){
-        return "index";
+    public String main(){
+        return "main";
     }
 
 }
