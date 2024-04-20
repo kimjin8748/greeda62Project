34143fd47e78ca3525cee483fa92a651cb69cf65
김용진 <kimjin8748@gmail.com>
2024-04-20 23:28:56 +0900

    04.20_validation 코드 삭제

--- src/main/java/inhatc/cse/springboot/greeda62project/dto/MemberDTO.java
+++ src/main/java/inhatc/cse/springboot/greeda62project/dto/MemberDTO.java
@@ -1,9 +1,7 @@
 package inhatc.cse.springboot.greeda62project.dto;
 
 import inhatc.cse.springboot.greeda62project.entity.MemberEntity;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
+
 import lombok.*;
 
 @Data
@@ -13,21 +11,19 @@ import lombok.*;
 @Builder
 public class MemberDTO {
 
-    @NotBlank(message = "아이디를 입력해주세요.")
+
     private String id;
 
-    @NotBlank(message = "비밀번호를 입력해주세요.")
-    @Size(min = 8, max = 20, message = "비밀번호는 8자 이상 20자 이하로 입력해주세요.")
+
     private String password;
 
-    @NotBlank(message = "이름을 입력해주세요.")
+
     private String name;
 
-    @NotBlank(message = "이메일을 입력해주세요.")
-    @Email(message = "올바른 이메일 주소를 입력해주세요.")
+
     private String email;
 
-    @NotBlank(message = "주소를 입력해주세요.")
+
     private String address;
 
     public static MemberDTO toMemberDTO(MemberEntity memberEntity) {
