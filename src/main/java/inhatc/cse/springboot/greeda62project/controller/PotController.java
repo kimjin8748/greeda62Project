626c132f4cd80738705f0e0b2ee5d4d7aacf1538
codingjs12 <kjsbest007@naver.com>
2024-04-22 16:18:57 +0900

    0422_카테고리 연결

--- src/main/java/inhatc/cse/springboot/greeda62project/controller/PotController.java
+++ src/main/java/inhatc/cse/springboot/greeda62project/controller/PotController.java
@@ -1,7 +1,9 @@
 package inhatc.cse.springboot.greeda62project.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class PotController {
     @GetMapping("/pot")
     public String pot() {
--- src/main/java/inhatc/cse/springboot/greeda62project/controller/SetController.java
+++ src/main/java/inhatc/cse/springboot/greeda62project/controller/SetController.java
@@ -1,7 +1,8 @@
 package inhatc.cse.springboot.greeda62project.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
+@Controller
 public class SetController {
     @GetMapping("/set")
     public String set() {
