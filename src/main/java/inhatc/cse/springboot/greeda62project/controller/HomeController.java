5354dac74ecb8dcc50e40d1c31691c071058cba8
김용진 <kimjin8748@gmail.com>
2024-05-03 16:37:42 +0900

    05.03_메인,모든상품쪽 화면에 보이기

--- src/main/java/inhatc/cse/springboot/greeda62project/controller/HomeController.java
+++ src/main/java/inhatc/cse/springboot/greeda62project/controller/HomeController.java
@@ -1,13 +1,30 @@
 package inhatc.cse.springboot.greeda62project.controller;
 
+import inhatc.cse.springboot.greeda62project.dto.ProductDTO;
+import inhatc.cse.springboot.greeda62project.dto.SucculentDTO;
+import inhatc.cse.springboot.greeda62project.entity.ProductEntity;
+import inhatc.cse.springboot.greeda62project.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
+@RequiredArgsConstructor
 public class HomeController {
 
+    private final ProductService productService;
+
     @GetMapping("/")
-    public String main(){
+    public String main(Model model){
+        List<ProductDTO> products = productService.findAllProducts();
+        List<SucculentDTO> succulents = productService.findSucProducts();
+
+        model.addAttribute("products", products);
+        model.addAttribute("succulents", succulents);
         return "main";
     }
 
