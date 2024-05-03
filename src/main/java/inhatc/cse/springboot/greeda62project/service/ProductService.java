4031959865eaf7b68d00e1490cfb3e9e231379c9
김용진 <kimjin8748@gmail.com>
2024-05-03 16:37:19 +0900

    05.03_상품 DB에서 가져오기 로직 구현

--- src/main/java/inhatc/cse/springboot/greeda62project/service/ProductService.java
+++ src/main/java/inhatc/cse/springboot/greeda62project/service/ProductService.java
@@ -1,4 +1,19 @@
 package inhatc.cse.springboot.greeda62project.service;
 
+import inhatc.cse.springboot.greeda62project.dto.PotDTO;
+import inhatc.cse.springboot.greeda62project.dto.ProductDTO;
+import inhatc.cse.springboot.greeda62project.dto.SetDTO;
+import inhatc.cse.springboot.greeda62project.dto.SucculentDTO;
+
+import java.util.List;
+
 public interface ProductService {
+
+    List<ProductDTO> findAllProducts();
+
+    List<PotDTO> findPotProducts();
+
+    List<SucculentDTO> findSucProducts();
+
+    List<SetDTO> findSetProducts();
 }
--- src/main/java/inhatc/cse/springboot/greeda62project/service/impl/ProductServiceImpl.java
+++ src/main/java/inhatc/cse/springboot/greeda62project/service/impl/ProductServiceImpl.java
@@ -1,6 +1,52 @@
 package inhatc.cse.springboot.greeda62project.service.impl;
 
+import inhatc.cse.springboot.greeda62project.dto.*;
+import inhatc.cse.springboot.greeda62project.entity.PotEntity;
+import inhatc.cse.springboot.greeda62project.entity.ProductEntity;
+import inhatc.cse.springboot.greeda62project.entity.SetEntity;
+import inhatc.cse.springboot.greeda62project.entity.SucculentEntity;
+import inhatc.cse.springboot.greeda62project.repository.PotRepository;
+import inhatc.cse.springboot.greeda62project.repository.ProductRepository;
+import inhatc.cse.springboot.greeda62project.repository.SetRepository;
+import inhatc.cse.springboot.greeda62project.repository.SucculentRepository;
 import inhatc.cse.springboot.greeda62project.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
 public class ProductServiceImpl implements ProductService {
+
+    private final ProductRepository productRepository;
+    private final PotRepository potRepository;
+    private final SucculentRepository succulentRepository;
+    private final SetRepository setRepository;
+
+    @Override
+    public List<ProductDTO> findAllProducts() {
+        List<ProductEntity> products = productRepository.findAll();
+        return products.stream().map(ProductDTO::toProductDTO).collect(Collectors.toList());
+    }
+
+    @Override
+    public List<PotDTO> findPotProducts() {
+        List<PotEntity> pots = potRepository.findAll();
+        return pots.stream().map(PotDTO::toPotDTO).collect(Collectors.toList());
+    }
+
+    @Override
+    public List<SucculentDTO> findSucProducts() {
+        List<SucculentEntity> Succulents = succulentRepository.findAll();
+        return Succulents.stream().map(SucculentDTO::toSucculentDTO).collect(Collectors.toList());
+    }
+
+    @Override
+    public List<SetDTO> findSetProducts() {
+        List<SetEntity> Sets = setRepository.findAll();
+        return Sets.stream().map(SetDTO::toSetDTO).collect(Collectors.toList());
+    }
 }
