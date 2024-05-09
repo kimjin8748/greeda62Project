9b52ea093cdee6c9a2122173cb6fee534756183e
김용진 <kimjin8748@gmail.com>
2024-05-09 13:30:15 +0900

    05.09_회원탈퇴시 confirm창으로 한번 더 확인하기 구현

--- src/main/resources/static/js/member_scripts.js
+++ src/main/resources/static/js/member_scripts.js
@@ -29,7 +29,20 @@ function pwCheck(){
     }
 }
 
+//탈퇴를 확인하는 메시지창을 띄우는 로직 추가
 function submitForm(actionType) {
-    document.getElementById('actionField').value = actionType; // action 값을 설정
-    document.forms[0].submit(); // 폼 제출
+    // 회원 탈퇴인 경우
+    if (actionType === 'delete') {
+        // confirm 창을 통해 사용자에게 삭제 의사를 확인
+        var confirmDelete = confirm("정말로 회원 탈퇴를 하시겠습니까?");
+        if (confirmDelete) {
+            // 사용자가 '확인'을 누른 경우 폼 제출
+            document.getElementById('actionField').value = 'delete';
+            document.getElementById('modifyForm').submit();
+        }
+    } else if (actionType === 'update') {
+        // 회원 정보 수정인 경우 바로 폼 제출
+        document.getElementById('actionField').value = 'update';
+        document.getElementById('modifyForm').submit();
+    }
 }
