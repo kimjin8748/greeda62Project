7a823cf099cd9df69892eae23d5978b355d66141
김용진 <kimjin8748@gmail.com>
2024-05-09 13:29:32 +0900

    05.09_불필요 클래스 삭제

--- src/main/java/inhatc/cse/springboot/greeda62project/dao/ProductDAO.java
+++
@@ -1,4 +0,0 @@
-package inhatc.cse.springboot.greeda62project.dao;
-
-public interface ProductDAO {
-}
--- src/main/java/inhatc/cse/springboot/greeda62project/dao/impl/ProductDAOImpl.java
+++
@@ -1,4 +0,0 @@
-package inhatc.cse.springboot.greeda62project.dao.impl;
-
-public class ProductDAOImpl {
-}
