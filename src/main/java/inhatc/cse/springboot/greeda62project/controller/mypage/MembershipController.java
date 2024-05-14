37d76c068155c420dcf12924073dead0e3e52bdd
김용진 <kimjin8748@gmail.com>
2024-05-14 21:48:39 +0900

    05.14_검색결과 없을때 구현중

--- src/main/java/inhatc/cse/springboot/greeda62project/controller/mypage/MembershipController.java
+++ src/main/java/inhatc/cse/springboot/greeda62project/controller/mypage/MembershipController.java
@@ -1,15 +1,8 @@
 package inhatc.cse.springboot.greeda62project.controller.mypage;
 
-import inhatc.cse.springboot.greeda62project.dto.MemberDTO;
-import inhatc.cse.springboot.greeda62project.service.MemberService;
 import jakarta.servlet.http.HttpSession;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-
-
-import java.util.List;
 
 @Controller
 public class MembershipController {
