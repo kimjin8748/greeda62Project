0c0d632aed718fa7268d502fa4b7945ddd048ae5
김용진 <kimjin8748@gmail.com>
2024-05-23 22:10:00 +0900

    05.23_관리자/회원관리 페이지 회원id 일부만 표시

--- src/main/java/inhatc/cse/springboot/greeda62project/dto/MemberDTO.java
+++ src/main/java/inhatc/cse/springboot/greeda62project/dto/MemberDTO.java
@@ -13,18 +13,19 @@ public class MemberDTO {
 
 
     private String id;
-
-
     private String password;
-
-
     private String name;
-
-
     private String email;
-
-
     private String address;
+    private String maskedMemberId;
+
+    public MemberDTO(String id, String password, String name, String email, String address) {
+        this.id = id;
+        this.password = password;
+        this.name = name;
+        this.email = email;
+        this.address = address;
+    }
 
     public static MemberDTO toMemberDTO(MemberEntity memberEntity) {
         MemberDTO dto = new MemberDTO();
@@ -34,6 +35,15 @@ public class MemberDTO {
         dto.setEmail(memberEntity.getEmail());
         dto.setAddress(memberEntity.getAddress());
         return dto;
+    }
+
+    public void setMaskedMemberId() {
+            if (id.length() > 4) {
+                this.maskedMemberId = id.substring(0, 4) + "*".repeat(id.length() - 4);
+            } else {
+                this.maskedMemberId = id;
+            }
+
     }
 
 }
