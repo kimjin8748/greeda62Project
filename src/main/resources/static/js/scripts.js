302d63d25494ca78b0e50793fa0933b3f26732b7
kimjin8748 <kimjin8748@gmail.com>
2025-01-05 15:35:50 +0900

    25.01.04_sweetalert2 라이브러리 일부 적용

--- src/main/resources/static/js/scripts.js
+++ src/main/resources/static/js/scripts.js
@@ -238,3 +238,38 @@ function cancelPayment() {
         });
 }
 
+/**
+ * 알림창 sweetalert2
+ * parameter: (필수) icon			=> 'info', 'success', 'error', 'warning', 'question'.
+ * 			  (필수) title			=> 알림 내용.
+ * 			  (선택) text			=> 알림 세부 내용. (없는 경우 비우거나 undefined)
+ * 			  (선택) confirmCallback	=> 확인 이후 진행할 function. (없는 경우 비우거나 undefined)
+ * 			  (선택) cancelCallback	=> 취소 이후 진행할 function. (없는 경우 비우거나 undefined)
+ * */
+function alert(icon, title, text, confirmCallback, cancelCallback) {
+    Swal.fire({
+        icon: icon,
+        title: title,
+        html: text.replace(/\n/g, '<br>'),
+        showConfirmButton: true,
+        confirmButtonColor: "#3085D6",
+        confirmButtonText: "확인",
+        showCancelButton: icon === 'question',
+        cancelButtonColor: "#DD3333",
+        cancelButtonText: "취소",
+        allowOutsideClick: false,
+        allowEscapeKey: false,
+        customClass: {
+            container: 'custom-alert-container' // 변경된 클래스
+        }
+    }).then(function(result) {
+        if (result.isConfirmed) { // 확인 시
+            if (confirmCallback !== undefined) confirmCallback();
+            return true;
+        } else if (result.isDenied) { // 취소 시
+            if (cancelCallback !== undefined) cancelCallback();
+            return false;
+        }
+    });
+}
+
